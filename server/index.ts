c6c46df2eda215653c4ac2ff9f55dda40fb59d21
0x706b <peter@peterkrol.com>
2021-10-11 20:09:55 -0400

    feat: add compression middleware

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import 'isomorphic-fetch'
 import { Console } from '@principia/base/IO/Console'
 import * as e from '@principia/express'
 import { runMain } from '@principia/node/Runtime'
+import compression from 'compression'
 import express from 'express'
 import path from 'path'
 
@@ -12,16 +13,19 @@ import * as Routes from './routes'
 
 const Manifest = makeManifestService('./dist/public/route-manifest.json')
 
-const globalMiddleware = e.use(
+const staticMiddleware = e.use(
   e.classic(express.static(path.join(__dirname, 'public'))),
 )
+
+const compressionMiddleware = e.use(e.classic(compression()))
 
 const Express = e.LiveExpressApp['<+<'](
   e.LiveExpressAppConfig('0.0.0.0', 8080, e.defaultExitHandler),
 )
 
 const program = IO.gen(function* (_) {
-  yield* _(globalMiddleware)
+  yield* _(compressionMiddleware)
+  yield* _(staticMiddleware)
   yield* _(Routes.SSR)
   const { server } = yield* _(e.ExpressApp)
   yield* _(Console.put('🚀 Express started on', server.address()))
